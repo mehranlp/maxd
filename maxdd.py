83daa69b31ef6b13495143a2ff9d87e052202478
mehranlp <65097550+mehranlp@users.noreply.github.com>
2021-01-07 19:22:49 -0500

    Update maxdd.py

--- maxdd.py
+++ maxdd.py
@@ -5,14 +5,14 @@ import plotly.express as px
 
 from dateutil.relativedelta import relativedelta
 """
-As of today, January 7, 2021, the stock market has been climbing and set a record higher almost every day. If you are following the tweeter feed, you will find a genius who made
-a 30-40% return in a day or week! Well, the purpose of this application is to give you a better sense of what you would have expected if you invest your money in the past,
-and how much your account could go down at any point in time when the market collapse. You can learn more about  [MAXIMUM DRAWDOWN here](https://www.investopedia.com/terms/d/drawdown.asp).
-The market timing is practically impossible and no one could tell what will happen in 1 day, let alone one week or year! But you may hear about "Long Term Return" which is on average 7% per year for S&p 500, that basically
-means if you have a long-term view, you should not worry about investment at any point, because the market always goes up in long term! As you can try with this application, 
-if you would invest in the S&P 500 in April 2000, you had to wait almost 13 years to get your initial investment back!!!Of course, this doesn't mean you should avoid investing 
+As of today, January 7, 2021, the stock market has been climbing and set a record higher almost every day. If you are following the Financial Twitter feed, you will find a GENIUS who beat the market and made
+a 30-40% return in a day or week! The market timing is practically impossible and no one could tell what will happen in 1 day, let alone one week or year! But you may hear about "Long Term Return" which is on average 7% per year for S&p 500, that basically
+means if you have a long-term view, you should not worry about investment at any point, because the market always goes up in long term! Well, the purpose of this application is to give you a better sense of what you would have expected if you invest your money in the past,
+and how much your account could go down at any point in time when the market collapse. You can learn more about  [maximum drawdown here](https://www.investopedia.com/terms/d/drawdown.asp).
+As you can try with this application, if you would invest in the S&P 500 in April 2000, you had to wait almost 13 years to get your initial investment back!!!Of course, this doesn't mean you should avoid investing 
 or try to time the market (again it is impossible!). It only means you must rely on your investment philosophy, asset diversification, and the company you want to invest in for
-return, not on the general market and the old idea of # Market Always Go Up. BY MEHRAN POORMALEK ([CODE REPOSITORY](https://github.com/mehranlp/maxd))
+return, not on the general market movements, and buy any stock you heard about on Twitter or motley ..... 
+BY MEHRAN POORMALEK ([CODE REPOSITORY](https://github.com/mehranlp/maxd))
 """
 
 # Create strart and end dates sidebar
